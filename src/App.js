fba98ff604453998d4d980147dec8139e09cb275
Harry Gothold <harrygot@hotmail.com>
2019-05-08 00:18:33 +0100

    finished validation, added search, registration

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
-import React, { Component } from 'react';
-import { Route, Redirect, Switch } from 'react-router-dom';
-import Movies from './components/movies';
-import MovieForm from './components/movieForm';
-import Customers from './components/customers';
-import Rentals from './components/rentals';
-import NotFound from './components/notFound';
-import NavBar from './components/navBar';
-import LoginForm from './components/loginForm';
-import './App.css';
+import React, { Component } from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
+import Movies from "./components/movies";
+import MovieForm from "./components/movieForm";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import NavBar from "./components/navBar";
+import LoginForm from "./components/loginForm";
+import "./App.css";
+import RegisterForm from "./components/registerForm";
 
 class App extends Component {
   render() {
     return (
       <>
         <NavBar />
-        <main className='container'>
+        <main className="container">
           <Switch>
-            <Route path='/login' component={LoginForm} />
-            <Route path='/movies/:id' component={MovieForm} />
-            <Route path='/movies' component={Movies} />
-            <Route path='/customers' component={Customers} />
-            <Route path='/rentals' component={Rentals} />
-            <Route path='/not-found' component={NotFound} />
-            <Redirect from='/' exact to='/movies' />
-            <Redirect to='/not-found' />
+            <Route path="/login" component={LoginForm} />
+            <Route path="/register" component={RegisterForm} />
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
           </Switch>
         </main>
       </>
